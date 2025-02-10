47d57fcd7908f0f0c7edbccca7206d7d036df93e
iamsomecow <145311683+iamsomecow@users.noreply.github.com>
2025-02-10 12:17:09 +1100

    add things

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
+function TaskTemplate(name = "new task") {
+return '<button type="button" onclick="taskButtonClick('+ taskCount.toString() +')">' + name +'</button>'
 
+}
+let taskData = {}
+let taskCount = 0
+function newTask() {
+    var taskobj = document.getElementById("tasks")
+    taskobj.appendChild(TaskTemplate())
+    taskData[taskCount.toString()].name = newTask;
+}
+function taskButtonClick(taskNumber) {
+    var menuobj = document.getElementById("menu")
+    
+}
