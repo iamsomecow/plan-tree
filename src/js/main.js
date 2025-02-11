43886bcbc62361399345c1272b28fdefb63ea297
iamsomecow <145311683+iamsomecow@users.noreply.github.com>
2025-02-11 12:51:07 +1100

    a

--- src/js/main.js
+++ src/js/main.js
@@ -6,10 +6,16 @@ let taskData = {}
 let taskCount = 0
 function newTask() {
     var taskobj = document.getElementById("tasks")
-    taskobj.appendChild(TaskTemplate())
-    taskData[taskCount.toString()].name = newTask;
+    taskobj.appendChild(document.createElement(TaskTemplate()))
+    taskData[taskCount.toString()].name = "new task";
+    taskData[taskCount.toString()].info = "this is a new task"
+    taskData[taskCount.toString()].status = "working on it"
 }
 function taskButtonClick(taskNumber) {
     var menuobj = document.getElementById("menu")
+    var Keys = taskData[taskNumber.toString()]
+    Keys.forEach(element => {
+        menuobj.appendChild(document.createElement('<p>' + taskData[taskNumber.toString()][element] + '</p>'))
+    });
     
 }
