590798a8c1b4c3c345b1286d1793d3cb94c4aae9
iamsomecow <145311683+iamsomecow@users.noreply.github.com>
2025-02-12 09:45:23 +1100

    a

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,28 @@
+let taskData = {};
+let taskCount = 0;
 function TaskTemplate(name = "new task") {
-return '<button type="button" onclick="taskButtonClick('+ taskCount.toString() +')">' + name +'</button>'
+return '<button type="button" onclick="taskButtonClick('+ taskCount.toString() +')">' + name +'</button>';
 
 }
-let taskData = {}
-let taskCount = 0
 function newTask() {
-    taskCount++
-    var taskobj = document.getElementById("tasks")
-    var e = document.createElement('div')
-    e.innerHTML = TaskTemplate()
-    taskobj.appendChild(e)
-    taskData[taskCount] = ["new task","this is a new task","working on it"]
-    console.log(taskData)
+    taskCount++;
+    var taskobj = document.getElementById("tasks");
+    var e = document.createElement('div');
+    e.innerHTML = TaskTemplate();
+    taskobj.appendChild(e);
+    taskData[taskCount] = ["new task","this is a new task","working on it"];
+    console.log(taskData);
 }
 function taskButtonClick(taskNumber) {
-    var menuobj = document.getElementById("menu")
-    menuobj.innerHTML = ""
+    var menuobj = document.getElementById("menu");
+    menuobj.innerHTML = "";
     taskData[taskNumber].forEach(element => {
-        var e = document.createElement('div')
-        e.innerHTML = element
-        menuobj.appendChild(e)
+        var e = document.createElement('div');
+        e.innerHTML = element;
+        menuobj.appendChild(e);
+        var a = document.createElement('button');
+        a.type = "button";
+        a.innerHTML = "edit";
+        menuobj.appendChild(a);
     });   
 }
